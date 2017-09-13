e31b9453ca24878a28891ffd51e13ed44b6cd69f
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-09-13 11:44:45 -0400

    Duel Project

--- DuelProjectGarrett/src/main/Duel.java
+++ DuelProjectGarrett/src/main/Duel.java
@@ -1,10 +1,34 @@
-package main;
+package duel;
+
+import duel.CharacterA;
+import duel.CharacterB;
+import duel.Duel;
+import duel.Dueler;
 
 public class Duel {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
+	private Dueler d1;
+	private Dueler d2;
+	
+	
+	
+	
+	public static void main(String[] args){
+		Duel duel = new Duel();
+		duel.dukeItOut();
 	}
+	
+	public Duel() {
+		d1 = new CharacterA();
+		d2 = new CharacterB();
+	}
+	
+	public void dukeItOut(){
+		
+	}
+	
 
 }
+
