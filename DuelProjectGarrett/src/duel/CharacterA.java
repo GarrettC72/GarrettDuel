117ee0cdc6589b852b9feacdabd9f595095a4ac3
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-09-14 11:48:03 -0400

    Updated CharacterA

--- DuelProjectGarrett/src/duel/CharacterA.java
+++ DuelProjectGarrett/src/duel/CharacterA.java
@@ -1,8 +1,29 @@
 package duel;
 
-public class CharacterA {
+public class CharacterA implements Dueler{
+	private int hp;
 	
-	public static void main(String[] args) {
+	public CharacterA() {
+		
+	}
+	
+	public String getName() {
+		return "Frederick";
+	}
+	
+	public void taunt() {
+		System.out.println("Pick a god and pray");
+	}
+	
+	public void setStartingHP(int hp) {
+		this.hp = hp;
+	}
+	
+	public int getHP() {
+		return hp;
+	}
+	
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
 		
 	}
 }
