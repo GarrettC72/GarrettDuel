024d30bd392dcebfdb9c4711368d63e35a0f7ed9
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-09-14 11:47:19 -0400

    Character B pre-boolean

--- DuelProjectGarrett/src/duel/CharacterB.java
+++ DuelProjectGarrett/src/duel/CharacterB.java
@@ -1,9 +1,30 @@
 package duel;
 
-public class CharacterB {
-
-	public static void main(String[] args) {
-
+public class CharacterB implements Dueler {
+	
+	private int hp;
+	
+	public CharacterB() {
+		
 	}
-
+	
+	public String getName() {
+		return "Douglass";
+	}
+	
+	public void taunt() {
+		System.out.println("Come at me, bro! Git Good");
+	}
+	
+	public void setStartingHP(int hp) {
+		this.hp = hp;
+	}
+	
+	public int getHP() {
+		return hp;
+	}
+	
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		
+	}
 }
